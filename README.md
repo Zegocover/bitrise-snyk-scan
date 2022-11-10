52244b72f46565a53513f70dde3fb6eb6a8a05bb
NataliaAn <natanastasiadi@gmail.com>
2022-11-10 11:20:04 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,110 @@
+# bitrise-snyk-scan
+Build step for integrating snyk to bitrise pipeline. It can run both on MacOS and Linux hosts. This step scans any project, either it's single or monorepo for code and dependency scanning.
+
+Dependencies managers supported:
+- Cocoapods
+- Gradle
+- Yarn
+- Npm
+
+The step's workflow is as follows:
+1. Fetch configuration
+2. Download and authenticate to Snyk
+3. Run Snyk SAST scan
+4. Run Dependency scan
+5. Fail step if any of the scan has findings or a failure occur (not blocking the build)
+
+## How to use this Step in your pipeline
+
+Create a workflow in your project's `bitrise.yml` as shown next
+
+```
+  workflow-snyk:
+    steps:
+    - git::https://github.com/Zegocover/bitrise-snyk-scan.git: 
+        title: Snyk
+        inputs:
+        - os_list: ios // is the project ios or android?
+        - severity_threshold: low // critical is not supported by'snyk code'
+        - org_name: some_name // Used to configure snyk organisation setting
+        - js_scan: false // Optional (set to true whenever needed) 
+```
+
+Then use this workflow as part of all other workflows you want to snyk scan e.g.
+
+```
+build-workflow:
+    before_run:
+    - workflow-clone
+    - ... // any other step before snyk scan
+    - workflow-snyk
+    - ... // any other step after snyk scan
+```
+
+For testing you can create a separate workflow which will clone the project's repo and then run the snyk step. Tie it as per usual to your test branch with a trigger. Once tested, use it in production pipelines.
+
+Do not forget to set your SNYK_AUTH_TOKEN in the secrets of the project.
+
+
+## How to use this Step locally
+
+Can be run directly with the [bitrise CLI](https://github.com/bitrise-io/bitrise),
+just `git clone` this repository, `cd` into it's folder in your Terminal/Command Line
+and call `bitrise run test`.
+
+*Check the `bitrise.yml` file for required inputs which have to be
+added to your `.bitrise.secrets.yml` file!*
+
+Step by step:
+
+1. Open up your Terminal / Command Line
+2. `git clone` the repository
+3. `cd` into the directory of the step (the one you just `git clone`d)
+5. Create a `.bitrise.secrets.yml` file in the same directory of `bitrise.yml`
+   (the `.bitrise.secrets.yml` is a git ignored file, you can store your secrets in it)
+6. Check the `bitrise.yml` file for any secret you should set in `.bitrise.secrets.yml`
+  * Best practice is to mark these options with something like `# define these in your .bitrise.secrets.yml`, in the `app:envs` section.
+7. Once you have all the required secret parameters in your `.bitrise.secrets.yml` you can just run this step with the [bitrise CLI](https://github.com/bitrise-io/bitrise): `bitrise run test`
+
+An example `.bitrise.secrets.yml` file:
+
+```
+envs:
+- SNYK_AUTH_TOKEN: your authentication token
+```
+
+# Future additions
+Swift package manager is going to be added when support will be released from Snyk.
+
+# Further dependencies manager support
+
+Here is an example of Maven support that has not been tested or integrated in the code of this plugin. If you need it, copy, test and debug any issues and open a PR to this step.
+
+```
+    # check if pom.xml exists
+    maven_file=$(find ${CODEFOLDER} -name 'pom.xml')
+
+    if [ -n "${maven_file}" ]
+    then
+        echo "Install maven and dependencies"
+        {
+            curl https://dlcdn.apache.org/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.tar.gz
+            mv apache-maven-3.8.4-bin.tar.gz apache-maven.tar.gz
+            tar -xvf apache-maven-3.8.4-bin.tar.gz -C /usr/local/apache-maven
+            export MAVEN_HOME=/usr/local/apache-maven/apache-maven
+            export PATH=$MAVEN_HOME/bin 
+
+            pom_path = $(echo ${maven_file}%/*)
+            cd ${pom_path}
+            mvn dependency:copy-dependencies
+        } || {
+            echo "!!! Unable to install maven"
+        }
+
+        echo "--- Running Android dependency scan"
+        ./snyk test --all-sub-projects --severity-threshold=${SEVERITY_THRESHOLD} 
+    else
+        echo "!!! No maven requirement file was found"
+    fi
+```
+
---
+++ step.sh
@@ -0,0 +1,184 @@
+#!/bin/bash
+set -eou pipefail
+
+# for swift and objective-c
+function snykscannerios-run() {
+    export GEM_HOME=$HOME/.gem
+    ruby_version="$(gem env | grep .gem/ruby | sed 's:.*.gem/::' | head -1)"
+    export PATH=$GEM_HOME/${ruby_version}/bin:$PATH
+
+    gem install cocoapods --user-install    
+    pod install # for podfile
+
+    bundle install # for gemfile
+
+    echo "--- Running iOS dependency scan"
+    ./snyk test --all-projects --severity-threshold=${severity_threshold}
+}
+
+# for java and kotlin
+function snykscannerandroid-run() {
+    # This needs maintenance to latest
+    echo "--- Install JDK"
+    if [[ $OSTYPE == 'darwin'* ]]; then
+        curl https://download.oracle.com/java/18/latest/jdk-18_macos-x64_bin.tar.gz --output jdk-18_macos-x64_bin.tar.gz
+        tar -xf jdk-18_macos-x64_bin.tar.gz
+    else
+        curl https://download.oracle.com/java/18/latest/jdk-18_linux-aarch64_bin.tar.gz --output jdk-18_linux-aarch64_bin.tar.gz 
+        tar -xf jdk-18_linux-aarch64_bin.tar.gz
+    fi
+
+    export JAVA_HOME=$(pwd)/$(find . -name Home | sed 's/^[^/\]*\///g') 
+    export PATH=$JAVA_HOME/bin:$PATH
+
+    build_gradle=$(find ${CODEFOLDER} -name 'build.gradle')
+
+    if [ -n "${build_gradle}" ]
+    then
+        echo "--- Running Android dependency scan"
+        ./snyk test --all-sub-projects --severity-threshold=${severity_threshold} 
+    else
+        echo '!!! No gradle requirement file was found'
+    fi
+}
+
+function bashversion() {
+    bash_version=$(bash --version | grep 1 | sed -nre 's/^[^0-9]*(([0-9]+\.)*[0-9]+).*/\1/p')
+    v1=$(echo ${bash_version} | cut -d'.' -f1)
+    v2=$(echo ${bash_version} | cut -d'.' -f2)
+
+    new_bash=0
+    if [ "$v1" -ge "4" ] && [ "$v2" -gt "3" ]; then
+        new_bash=1
+    fi
+
+    echo "$new_bash"
+}
+
+function snykscannerjs-run() {
+    echo "--- Downloading and installing project javascript dependencies."
+
+    new_bash=$(bashversion)
+
+    echo "--- Checking all yarn.lock files in the project"
+    yarn_files=()
+    if [ "$new_bash" -eq "1" ]; then 
+        yarn_files="$(find ${CODEFOLDER} -name 'yarn.lock' -print0)"
+        readarray -d ' ' yarn_files < <(echo ${yarn_files//"yarn.lock"/" "})
+    else
+        while IFS=  read -r -d $'\0'; do
+            yarn_files+=("$REPLY")
+        done < <(find ${CODEFOLDER} -name 'yarn.lock')
+    fi
+
+    len=${yarn_files[@]};
+    if [[ len -gt 0 ]]; then
+        echo "--- Running yarn installation"
+        for i in "${yarn_files[@]}"
+        do
+            cd $i
+            echo "Running yarn install for $i"
+            yarn install
+        done
+        cd ${CODEFOLDER}
+    fi
+
+    echo "--- Checking all package-lock.json files in the project"
+    npm_files=()
+    if [ "$new_bash" -eq "1" ]; then 
+        npm_files="$(find ${CODEFOLDER} -name 'package-lock.json' -print0)"
+        readarray -d ' ' npm_files < <(echo ${npm_files//"package-lock.json"/" "})
+    else
+        # this has not been tested - might fail
+        while IFS=  read -r -d $'\0'; do
+        yarn_files+=("$REPLY")
+        done < <(find ${CODEFOLDER} -name 'package-lock.json')
+    fi
+
+    en=${npm_files[@]};
+    if [[ len -gt 0 ]]; then
+        echo "--- Running npm installation"
+        for i in "${npm_files[@]}"
+        do
+            cd $i
+            echo "Running npm install for $i"
+            npm install
+        done
+        cd ${CODEFOLDER}
+    fi
+
+    echo "--- Running javascript dependency scan"
+    ./snyk test --all-projects --severity-threshold=${severity_threshold} 
+}
+
+
+
+function main(){
+    ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+
+    CODEFOLDER=${BITRISE_SOURCE_DIR}
+
+    if [[ -z "${os_list}" ]]
+    then
+        echo "os input is not valid"
+        exit 1
+    fi
+
+    if [[ -z "${org_name}" ]]
+    then
+        echo "org_name input is not valid"
+        exit 1
+    fi
+
+    echo "+++ Running Snyk Vulnerability Scanner Pipeline Step."
+
+    echo "--- Identifying the application to scan."
+    echo "Project path: ${CODEFOLDER}"
+
+    echo "--- Downloading Snyk CLI"
+    if [[ $OSTYPE == 'darwin'* ]]; then
+        echo "OS: MacOS"
+        curl https://static.snyk.io/cli/latest/snyk-macos -o snyk
+    else
+        echo "OS: Linux"
+        curl https://static.snyk.io/cli/latest/snyk-linux -o snyk
+    fi
+
+    chmod +x ./snyk
+    echo "--- Authenticating to Snyk."
+    ./snyk auth ${snyk_auth_token} 
+    ./snyk config set org=${org_name}
+
+    # check if there are SAST findings
+    sast_findings=false
+    echo "--- Running code analysis scan" 
+    {
+        ./snyk code test --severity-threshold=${severity_threshold}
+    } || {
+        sast_findings=true
+    }
+
+    # check if there are dependency findings
+    dep_findings=false
+    {
+        if [[ ${os_list} == "ios" ]]; then
+            snykscannerios-run
+        else
+            snykscannerandroid-run
+        fi
+
+        if [[ ${js_scan} == "true" ]]; then
+            snykscannerjs-run
+        fi
+    } || {
+        dep_findings=true
+    }
+
+    if [ "$sast_findings" == "true" ] || [ "$dep_findings" == "1" ]; then
+        exit 1;
+    else
+        exit 0;
+    fi
+}
+
+main

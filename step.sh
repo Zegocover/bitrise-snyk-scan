14959c85e72dcab7bcb9063dc51f9254d40a8bfd
NataliaAn <natanastasiadi@gmail.com>
2022-11-30 11:26:17 +0000

    remove gradlew call

--- step.sh
+++ step.sh
@@ -21,11 +21,8 @@ function snykscannerandroid-run() {
     echo "--- Install gradle"
     curl https://downloads.gradle-dn.com/distributions/gradle-7.5.1-bin.zip --output gradle-7.5.1-bin.zip
     unzip -d /opt/gradle gradle-7.5.1-bin.zip
-    ls /opt/gradle
 
     export PATH=$PATH:/opt/gradle/gradle-7.5.1/bin
-    ls /opt/gradle/gradle-7.5.1/bin
-    chmod +x gradlew
 
     build_gradle=$(find ${CODEFOLDER} -name 'build.gradle')
 

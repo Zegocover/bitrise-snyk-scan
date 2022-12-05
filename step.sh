66f1b3913379c1492fc404b72458459203e2fbe4
NataliaAn <natanastasiadi@gmail.com>
2022-12-05 11:43:52 +0000

    remove debugging

--- step.sh
+++ step.sh
@@ -78,7 +78,7 @@ function snykscannerandroid-run() {
     len=${#gradle_files[@]};
     if [[ len -gt 0 ]]; then
         echo $scan_print
-        ./snyk test --all-projects --severity-threshold=${severity_threshold} -d
+        ./snyk test --all-projects --severity-threshold=${severity_threshold}
     else
         echo '!!! No gradle requirement file was found'
     fi
